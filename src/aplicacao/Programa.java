f1bb9d0f53b02a298d70657a8c56f6212d57951e
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-05 02:24:37 -0300

    Projeto contagem de votos

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,30 +1,61 @@
 package aplicacao;
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.LinkedHashMap;
 import java.util.Map;
-
-import entities.Product;
+import java.util.Scanner;
 
 public class Programa {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-		Map<Product, Double> stock = new HashMap<>();
+		Scanner sc = new Scanner(System.in);
 		
-		Product p1 = new Product("Tv", 900.0);
-		Product p2 = new Product("Notebook", 1200.0);
-		Product p3 = new Product("Tablet", 400.0);
+
+		Map<String, Integer> votos = new LinkedHashMap<>();
+
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+				
+				String[] fields = line.split(",");
+				String nome = fields[0];
+				int quantidade = Integer.parseInt(fields[1]);
+
+				if (votos.containsKey(nome)) {
+					int votesSoFar = votos.get(nome);
+					votos.put(nome, quantidade + votesSoFar);
+				}
+				else {
+					votos.put(nome, quantidade);
+				}
+				
+				line = br.readLine();
+			}
+			
+			for (String key : votos.keySet()) {
+				System.out.println(key + ": " + votos.get(key));
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		sc.close();
+	}
 		
-		stock.put(p1, 10000.0);
-		stock.put(p2, 20000.0);
-		stock.put(p3, 15000.0);
 		
-		Product ps = new Product("Tv", 900.0);
-		
-		System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
+	
 		
 		}
 
-	}
+	
 
 

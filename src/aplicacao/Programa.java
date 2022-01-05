d02d80937d0f8a0350be2d4fa9b772b5d79ebd95
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-05 00:41:43 -0300

    Map

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -12,12 +12,19 @@ public class Programa {
 		cookies.put("username", "Maria");
 		cookies.put("email", "maria@gmail.com");
 		cookies.put("phone","99711122");
+		cookies.put("rg","1010911018");
 		
 		cookies.remove("email");
+		cookies.put("phone","99771133");
+		
+		System.out.println("Contem 'phone' key: " + cookies.containsKey("CPF"));
+		System.out.println("phone number: " + cookies.get("phone"));
+		System.out.println("Email: " + cookies.get("email"));
+		System.out.println("Size: " + cookies.size());
 		
 		System.out.println("TODOS COOKIES:");
 		for(String key : cookies.keySet()) {
-			System.out.println(key + ": " + cookies.get(key));
+			System.out.println(key + "= " + cookies.get(key));
 		}
 
 	}
